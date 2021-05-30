9d98f7805e57dc14309e63b1938ad93ed9bb7788
Wellington Gertrudes do Nascimento <wellingtonTgertrudes@gmail.com>
2021-05-30 12:48:11 -0700

    Chess layer and  printing the board

--- Projeto/src/boardgame/Board.java
+++ Projeto/src/boardgame/Board.java
@@ -27,4 +27,12 @@ public class Board {
     public void setColumn(int column) {
         this.column = column;
     }
+
+    public Piece piece (int rows, int column) {
+        return pieces [rows] [column];
+    }
+
+    public Piece piece (Position position) {
+        return pieces [position.getRow()] [position.getColumn()];
+    }
 }
