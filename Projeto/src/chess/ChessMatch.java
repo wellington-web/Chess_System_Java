1038fe870b7fe1b3751d4828a64384d0eb2aa0c8
Wellington Gertrudes do Nascimento <wellingtonTgertrudes@gmail.com>
2021-05-30 22:52:18 -0700

    ChessException and ChessPosition

--- Projeto/src/chess/ChessMatch.java
+++ Projeto/src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -24,9 +23,13 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row , ChessPiece piece) {
+        board.placePiece(piece,new ChessPosition(column,row).toPosition());
+    }
+
     private void initialSetup () {
-        board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
-        board.placePiece(new King(board,Color.BLACK),new Position(4,3));
-        board.placePiece(new King(board,Color.WHITE),new Position(5,7));
+        placeNewPiece('b',6, new Rook(board,Color.WHITE));
+        placeNewPiece('c',5, new King(board,Color.BLACK));
+        placeNewPiece('d',3, new King(board,Color.WHITE));
     }
 }
