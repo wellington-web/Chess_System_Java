16fbb2065183905f57d7f1759394e7e527acb4e1
Wellington Gertrudes do Nascimento <wellingtonTgertrudes@gmail.com>
2021-06-22 05:41:46 -0700

    Moving pieces

--- Projeto/src/application/App.java
+++ Projeto/src/application/App.java
@@ -1,12 +1,30 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner (System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.pieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.pieces());
+            System.out.println();
+            System.out.print("source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturePiece = chessMatch.performChessMove(source, target);
+
+        }
     }
 }
